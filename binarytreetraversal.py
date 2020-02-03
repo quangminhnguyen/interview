94e7ed4a41983023bbd15d5adf9406fd1fdfef19
Nguyen Quang Minh <quangminh.nguyen@mail.utoronto.ca>
2020-02-03 11:45:13 -0700

    Implemented tree operations  with recursion

--- binarytreetraversal.py
+++ binarytreetraversal.py
@@ -37,6 +37,43 @@ def pre_order( tree, i ):
 		result = result + in_order( tree, 2 * i + 2 );
 	return result;
 
+'''
+	Calculates depth of a tree
+	Parameters:
+		tree - A complete binary search tree represented by an array
+		i - index of root node
+	Return:
+		depth of the given tree
+'''
+def calculate_depth( tree, i ):
+	if ( ( 2 * i + 1 ) >= len( tree ) and ( 2 * i + 2 ) >= len( tree ) ):
+		return 0;
+	elif ( ( 2 * i + 1 ) >= len(tree) ):
+		return calculate_depth( tree, 2 * i + 2 ) + 1;
+	elif ( ( 2 * i + 2 ) >= len(tree) ):
+		return calculate_depth( tree, 2 * i + 1 ) + 1;
+	else:
+		return max( calculate_depth( tree, 2 * i + 1 ) + 1, calculate_depth( tree, 2 * i + 2 ) + 1 );
+		
+
+
+'''
+	Calculates number of leaves in the tree 
+	Parameters:
+		tree - A binary tree
+		i - index of the current root node 
+	Return:
+		number of leaves in the tree
+'''
+def count_leave( tree, i ):
+	if ( ( 2 * i + 1 ) >= len( tree ) and ( 2 * i + 2 ) >= len( tree ) ):
+		return 1;
+	elif ( ( 2 * i + 1 ) >= len(tree) ):
+		return count_leave( tree, 2 * i + 2 );
+	elif ( ( 2 * i + 2 ) >= len(tree) ):
+		return count_leave( tree, 2 * i + 1 );
+	else:
+		return count_leave( tree, 2 * i + 2 ) + count_leave( tree, 2 * i + 1 );
 
 
 if __name__ == "__main__":
@@ -46,3 +83,5 @@ if __name__ == "__main__":
 	
 	print( "In-order tree traversal", in_order( bst, 0 ) );	
 	print( "Pre-order tree traversal", pre_order( bst, 0 ) );
+	print( "Tree-depth", calculate_depth( bst, 0 ) );
+	print( "Count number of leaves", count_leave( bst, 0 ) );
